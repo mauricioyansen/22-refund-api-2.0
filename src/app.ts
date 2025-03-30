c0b5c8dd5eae0615d1544ac7d5ead3494b2cab26
Mauricio <mauricio_cy@hotmail.com>
2025-03-30 00:57:28 -0300

    file preview

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,15 @@ import express from "express";
 import cors from "cors";
 import { errorHandling } from "./middlewares/error-handling";
 import { routes } from "./routes";
+import uploadConfig from "./config/upload";
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
